c1e6ba14ea2bbb3e6a862ae0fe4151a480a3903a
Sr_lucky_MaN <d.dunin1709@gmail.com>
2026-08-23 19:31:47 +0500

    import fix

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from src.routers.auth import router as auth_router
 from src.routers.proxy import router as proxy_router
 from src.routers.health import router as health_router
-
+from src.config import settings
 app = FastAPI(root_path=settings.root_path)
 
 app.include_router(auth_router, prefix="/auth")
